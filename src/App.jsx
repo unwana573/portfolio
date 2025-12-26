ec2e5fae71ef7a8d9722db6f7e8ce8f746ec93a0
unwana orok <orokunwana@gmail.com>
2025-12-26 18:02:46 +0100

    quick fix

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
+import Index from "./pages/index";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
